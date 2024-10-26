9d43a08f25dff0c19577a2f17e79aed086190c00
maxhaesslein <git@maxhaesslein.de>
2024-10-26 10:05:17 +0200

    cleaned up the server code a bit

--- server/server.js
+++ server/server.js
@@ -7,23 +7,28 @@ let clients = [];
 
 server.on('connection', (socket) => {
 
-	console.log('new connection');
-
+	console.log( 'new connection' );
 	clients.push(socket);
+	console.log( '  now having', clients.length, 'clients' );
 
 	socket.on('message', (message) => {
-		console.log('new message');
+		//console.log('new message with a length of', message.length);
 		clients.forEach((client) => {
-			if (client !== socket && client.readyState === WebSocket.OPEN) {
-				client.send(message);
-			}
+
+			if( client === socket ) return;
+			if( client.readyState !== WebSocket.OPEN ) return;
+
+			client.send(message);
+
 		});
 	});
 
 	socket.on('close', () => {
 		console.log('connection closed');
 		clients = clients.filter((client) => client !== socket);
+		console.log( '  now having', clients.length, 'clients' );
 	});
+
 });
 
 console.log("WebSocket signaling server running on ws://localhost:"+port);
